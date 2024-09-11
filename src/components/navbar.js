39dddc4ae77c62c81da096f0bd8be1858a2f3f0c
Felipe-LasHeras <FE.LASHERAS@DUOCUC.CL>
2024-09-11 16:55:27 -0300

    cambion navbar y agregado nuevas paginas

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import logo from "../assets/logo/Logo_ActoCero_en_blanco__1__page-0001-removebg-preview.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          <img src={logo} alt="Actocero" style={{ height: '100%', maxHeight: '60px' }} />
         </a>
         <button
           className="navbar-toggler"
@@ -21,18 +23,28 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
+              <Link className="nav-link text-white" to="/Presentacion">
+                Actocero 
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Integrantes
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
+              <a className="nav-link text-white" href="#">
+                Obras
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
+              <a className="nav-link text-white" href="#">
+                Obras
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Contacto
               </a>
             </li>
           </ul>
